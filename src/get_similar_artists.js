fa7d5246e708fb91619d70ff4938b3fdb3949afe
Michael Shafer <mshafer3@gmail.com>
2018-09-16 21:50:37 +1000

    Initial commit with get_similar_artists script

---
+++ src/get_similar_artists.js
@@ -0,0 +1,71 @@
+import * as Songkick from "songkick-api-node";
+import * as fs from 'fs';
+
+/**
+ * Given a songkick username, fetch the artists they track, and all similar artists.
+ * The output is written to artists.json, taking the following format:
+ * 
+ * {
+ *     username: "user1"
+ *     trackedArtists: [
+ *         { 
+ *             <standard Songkick artist properties> 
+ *         },
+ *         ...
+ *     ],
+ *     recommendedArtists: [
+ *         {
+ *             <standard Songkick artist properties>,
+ *             similarTo: [ <list of artists IDs they matched on> ]
+ *         },
+ *         ...
+ *     ]
+ * }
+ */
+
+const OUTPUT_FILE = "artists.json";
+const API_KEY = process.env.SONGKICK_API_KEY;
+const songkickApi = new Songkick(API_KEY);
+
+function sleep (time) {
+    console.log(`Sleeping for ${time}ms`);
+    return new Promise((resolve) => setTimeout(resolve, time));
+}
+
+async function getSimilarArtists(artists) {
+    const artistIds = artists.map(a => a.id);
+    const similarArtistsById = {};
+    for (let artistId of artistIds) {
+        const similarArtists = songkickApi.getArtistSimilar(artistId);
+        for (const similarArtist of similarArtists) {
+            if (!similarArtistsById.hasOwnProperty(similarArtist.id)) {
+                similarArtistsById[similarArtist.id] = similarArtist;
+                similarArtistsById[similarArtist.id].similarTo = [artist.id];
+            } else {
+                similarArtistsById[similarArtist.id].similarTo.push(artist.id);
+            }
+        }
+        await sleep(200);
+    }
+    return similarArtistsById.values();
+}
+
+async function main() {
+    if (process.argv.length < 3) {
+        console.error(`Usage: node ${process.argv[1]} <songkick_username>`);
+        throw new Error("Invalid number of arguments provided to program");
+    }
+    const username = process.argv[2];
+    const trackedArtists = songkickApi.getUserTrackedArtists(username);
+    const recommendedArtists = await getSimilarArtists(trackedArtists);
+    const result = {
+        username,
+        trackedArtists,
+        recommendedArtists
+    };
+    fs.writeFileSync(OUTPUT_FILE, JSON.stringify(result));
+}
+
+if (require.main === module) {
+    await main();
+}
