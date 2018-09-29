feef0ce7cd1497959183de239ad60e92d7cceaa5
Michael Shafer <mshafer3@gmail.com>
2018-09-29 17:42:16 +1000

    Add documentation to explain top-level entry script start_act.js

--- src/start_act.js
+++ src/start_act.js
@@ -2,6 +2,11 @@ import 'babel-polyfill';
 const Apify = require("apify");
 import { runFetchTrackedAndSimilarArtists } from "./acts/fetch_tracked_and_similar_artists";
 
+/**
+ * Each Apify Actors runs this script, setting the environment variable `ACT_NAME` to choose
+ * which act function to call.
+ */
+
 const ACT_NAME = process.env.ACT_NAME;
 let actFunction;
 
